4c61106dfdee16aa3044ddfd89e754ed8eae12b9
Maurício Covolan Rosito <mauricio.rosito@bento.ifrs.edu.br>
2025-04-28 19:03:43 -0300

    Adiciona função mostrarHoraAtual no index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log("Meu primeiro projeto Node.js usando Git!");
 console.log("Alteração: Adicionei uma nova linha!");
+
+function mostrarHoraAtual() {
+  const agora = new Date();
+  console.log(`Horário atual: ${agora.toLocaleTimeString()}`);
+}
+
+mostrarHoraAtual();
